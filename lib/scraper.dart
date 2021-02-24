558934b73d6c78c2d52c63ed3a3ec3a08b9ed9c7
Dominic <db2002dominic@gmail.com>
2021-02-24 23:25:23 +0100

    csvToMap() and getCurrentGreenEnergyPercentage()

--- lib/scraper.dart
+++ lib/scraper.dart
@@ -1,12 +1,123 @@
 import 'package:http/http.dart';
 import 'dart:convert';
-import 'package:df/df.dart';
+import 'package:csv/csv.dart';
+
+// power generation
+List REALIZED_POWER_GENERATION = [
+  1001224,
+  1004066,
+  1004067,
+  1004068,
+  1001223,
+  1004069,
+  1004071,
+  1004070,
+  1001226,
+  1001228,
+  1001227,
+  1001225
+];
+List INSTALLED_POWER_GENERATION = [
+  3004072,
+  3004073,
+  3004074,
+  3004075,
+  3004076,
+  3000186,
+  3000188,
+  3000189,
+  3000194,
+  3000198,
+  3000207,
+  3003792
+];
+List FORECASTED_POWER_GENERATION = [
+  2000122,
+  2000715,
+  2000125,
+  2003791,
+  2000123
+];
+
+// power consumption
+List FORECASTED_POWER_CONSUMPTION = [6000411, 6004362];
+List REALIZED_POWER_CONSUMPTION = [5000410, 5004359];
+
+// market
+List WHOLESALE_PRICES = [
+  8004169,
+  8004170,
+  8000252,
+  8000253,
+  8000251,
+  8000254,
+  8000255,
+  8000256,
+  8000257,
+  8000258,
+  8000259,
+  8000260,
+  8000261,
+  8000262
+];
+List COMMERCIAL_FOREIGN_TRADE = [
+  8004169,
+  8004170,
+  8000252,
+  8000253,
+  8000251,
+  8000254,
+  8000255,
+  8000256,
+  8000257,
+  8000258,
+  8000259,
+  8000260,
+  8000261,
+  8000262
+];
+List PHYSICAL_POWER_FLOW = [
+  31000714,
+  31000140,
+  31000569,
+  31000145,
+  31000574,
+  31000570,
+  31000139,
+  31000568,
+  31000138,
+  31000567,
+  31000146,
+  31000575,
+  31000144,
+  31000573,
+  31000142,
+  31000571,
+  31000143,
+  31000572,
+  31000141
+];
+
+Map<String, List<dynamic>> csvToMap(String csv) {
+  List<List<dynamic>> csvList =
+      CsvToListConverter().convert(csv, fieldDelimiter: ';');
+  Map<String, List<dynamic>> result = Map();
+
+  for (int i = 0; i < (csvList[0].length); i++) {
+    List data = [];
+    for (int j = 1; j < (csvList.length - 1); j++) {
+      data.add(csvList[j][i]);
+    }
+    result.putIfAbsent(csvList[0][i], () => data);
+  }
+  return result;
+}
 
 class SmardData {
   SmardData();
 
   Future<String> requestData(
-      {int fromInHoursAgoNow = 5,
+      {int fromInHoursAgoNow = 24,
       int toInHoursAfterNow = 0,
       Modul modul = Modul.price}) async {
     var time = DateTime.now().millisecondsSinceEpoch;
@@ -16,10 +127,16 @@ class SmardData {
     List modules;
     switch (modul) {
       case Modul.consumption:
-        modules = [6000411, 6004362];
+        modules = FORECASTED_POWER_CONSUMPTION;
         break;
       case Modul.price:
-        modules = [8004169];
+        modules = WHOLESALE_PRICES[0];
+        break;
+      case Modul.generation:
+        modules = REALIZED_POWER_GENERATION;
+        break;
+      case Modul.forecasted_generation:
+        modules = FORECASTED_POWER_GENERATION;
         break;
     }
 
@@ -45,10 +162,68 @@ class SmardData {
     return response.body;
   }
 
-  //int getCurrentGreenEnergyPercentage() {}
+  Future<double> getCurrentGreenEnergyPercentage() async {
+    Map generation = csvToMap(await requestData(modul: Modul.generation));
+    Map forecastedGeneration =
+        csvToMap(await requestData(modul: Modul.forecasted_generation));
+
+    List renewable = [
+      'Biomasse[MWh]',
+      'Wasserkraft[MWh]',
+      'Sonstige Erneuerbare[MWh]'
+    ];
+    List conventional = [
+      'Kernenergie[MWh]',
+      'Braunkohle[MWh]',
+      'Steinkohle[MWh]',
+      'Erdgas[MWh]',
+      'Pumpspeicher[MWh]',
+      'Sonstige Konventionelle[MWh]'
+    ];
+    List forecastedRenewable = [
+      "Wind Offshore[MWh]",
+      "Wind Onshore[MWh]",
+      "Photovoltaik[MWh]"
+    ];
+    double ePowerGen = 0.0;
+    double kPowerGen = 0.0;
+
+    renewable.forEach((index) {
+      double energy;
+      List gen = generation[index];
+      for (int i = 1; i < gen.length; i++) {
+        if (gen[gen.length - i] != "-") {
+          energy = gen[gen.length - i].toDouble();
+          break;
+        }
+      }
+      ePowerGen += energy;
+    });
+    forecastedRenewable.forEach((index) {
+      ePowerGen += forecastedGeneration[index].last;
+    });
+    conventional.forEach((index) {
+      double energy;
+      List gen = generation[index];
+      for (int i = 1; i < gen.length; i++) {
+        if (gen[gen.length - i] != "-") {
+          energy = gen[gen.length - i].toDouble();
+          break;
+        }
+      }
+      kPowerGen += energy;
+    });
+
+    double percentage = double.parse(
+        (ePowerGen / (ePowerGen + kPowerGen) * 100).toStringAsFixed(1));
+
+    return percentage;
+  }
 }
 
 enum Modul {
   price,
   consumption,
+  generation,
+  forecasted_generation,
 }
